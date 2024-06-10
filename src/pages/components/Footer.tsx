45e2fc144b7049b133ed56f91b792c7dfe77c689
Reza Aghajani <rezaaghajani@protonmail.com>
2024-06-10 22:12:20 +0330

    Landing Page Done

---
+++ src/pages/components/Footer.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import me from "../../../public/me.jpg";
+import Image from "next/image";
+import { FaInstagram } from "react-icons/fa";
+import { TbBrandTwitter } from "react-icons/tb";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer w-full h-[30vh] font-peyda flex flex-col justify-center items-center gap-4">
+        <Image
+          src={me}
+          alt="Reza Aghajani"
+          width={80}
+          height={80}
+          className="rounded-full"
+        />
+        <div className=" flex flex-col justify-center items-center">
+          <h1 className="font-bold text-xl">رضا آقاجانی</h1>
+          <h1>ساخته شده بدون ❤️</h1>
+        </div>
+
+        <div className="social-media flex flex-row gap-4">
+          <Link href={"instagram.com/rezasmind"}>
+            <FaInstagram color="#66D7D1" size={20}/>
+          </Link>
+
+          <Link href={"twitter.com/rezasmind"}>
+            <TbBrandTwitter color="#66D7D1" size={20}/>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
