939f80752e5765bde5cd6262825270038ebae0fb
Reza Aghajani <rezaaghajani@protonmail.com>
2024-09-20 15:07:02 +0330

    Fix APi

--- src/pages/api/auth/send-otp.ts
+++ src/pages/api/auth/send-otp.ts
@@ -19,9 +19,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method Not Allowed" });
-  }
+
 
   const { phoneNumber } = req.body;
 
